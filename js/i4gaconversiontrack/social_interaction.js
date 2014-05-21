30e5623713fb3741428e2d420a36cadc1136c375
Bas <basblanken@gmail.com>
2014-05-21 17:24:31 +0200

    todos added for FB share tracking

--- js/i4gaconversiontrack/social_interaction.js
+++ js/i4gaconversiontrack/social_interaction.js
@@ -1,7 +1,6 @@
 window.fbAsyncInit = function() {
     // FaceBook `like` clicked
     if(social_interaction_track_fb_like){
-        alert('social_interaction_track_fb_like');
         FB.Event.subscribe('edge.create', function(targetUrl) {
             ga('send', 'social', 'facebook', 'like', targetUrl);
         });
@@ -9,13 +8,12 @@ window.fbAsyncInit = function() {
 
     // FaceBook `unlike` clicked
     if(social_interaction_track_fb_unlike){
-        alert('social_interaction_track_fb_unlike');
         FB.Event.subscribe('edge.remove', function(targetUrl) {
             ga('send', 'social', 'facebook', 'unlike', targetUrl);
         });
     }
 
-    // FaceBook share
+    /* @TODO: This one needs work! */
     if(social_interaction_track_fb_share){
         FB.Event.subscribe('message.send', function(targetUrl) {
             ga('send', 'social', 'facebook', 'send', targetUrl);
